1510f1d6c5400874fc40cfe5b05e9abe64a5e63d
Philip LeBlond <philbert@brugenhagen.com>
2021-04-13 18:23:04 -0400

    First build

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 15);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "tock";
+            this.lblTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Hide";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(260, 112);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTime);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "The time... roughly";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Test
+{
+
+        public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            //notifyIcon1.BalloonTipText = "Loading.";
+            //notifyIcon1.BalloonTipTitle = "Title";
+            //notifyIcon1.ShowBalloonTip(2000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string timeoutput = "";
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = label1.Text;
+            notifyIcon1.BalloonTipTitle = "The time";
+            notifyIcon1.ShowBalloonTip(2000);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime nowish = DateTime.Now;
+            int roundup = 0;
+            int nearfifteen = 0;
+            if (nowish.Minute >= 38) { roundup = 1; }
+            else { roundup = 0; }
+            string strHour = "NaN";
+            string strMinute = "NaN";
+            string strMorNoonNight = "NaN";
+
+            if (nowish.Minute <= 7 || nowish.Minute > 53) { nearfifteen = 0; }
+            else { nearfifteen = 9; }
+            
+            if (nowish.Minute >7 && nowish.Minute <= 22) { nearfifteen = 1; }
+            if (nowish.Minute >22 && nowish.Minute <=37) { nearfifteen = 2; }
+            if (nowish.Minute >37 && nowish.Minute <=53) { nearfifteen = 3; }
+           
+           
+            switch(nearfifteen)
+            {
+                case 0:
+                    strMinute = "It's ";
+                    break;
+                case 1:
+                    strMinute = "It's quarter past ";
+                    break;
+                case 2:
+                    strMinute = "It's half past ";
+                    break;
+                case 3:
+                    strMinute = "It's quarter till ";
+                    break;
+                case 9:
+                    strMinute = "Time is broken.";
+                    break;
+            }
+
+            switch ((nowish.Hour + roundup))
+            {
+                case 1: 
+                    strHour = "one";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 2:
+                    strHour = "two";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 3:
+                    strHour = "three";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 4:
+                    strHour = "four";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 5:
+                    strHour = "five";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 6:
+                    strHour = "six";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 7:
+                    strHour = "seven";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 8:
+                    strHour = "eight";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 9:
+                    strHour = "nine";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 10:
+                    strHour = "ten";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 11:
+                    strHour = "eleven";
+                    strMorNoonNight = " in the Morning";
+                    break;
+                case 12:
+                    strHour = "noon";
+                    strMorNoonNight = "";
+                    break;
+                case 13:
+                    strHour = "one";
+                    strMorNoonNight = " in the afternoon";
+                    break;
+                case 14:
+                    strHour = "two";
+                    strMorNoonNight = " in the afternoon";
+                    break;
+                case 15:
+                    strHour = "three";
+                    strMorNoonNight = " in the afternoon";
+                    break;
+                case 16:
+                    strHour = "four";
+                    strMorNoonNight = " in the afternoon";
+                    break;
+                case 17:
+                    strHour = "five";
+                    strMorNoonNight = " in the afternoon";
+                    break;
+                case 18:
+                    strHour = "six";
+                    strMorNoonNight = " in the afternoon";
+                    break;
+                case 19:
+                    strHour = "seven";
+                    strMorNoonNight = " in the evening";
+                    break;
+                case 20:
+                    strHour = "eight";
+                    strMorNoonNight = " in the evening";
+                    break;
+                case 21:
+                    strHour = "nine";
+                    strMorNoonNight = " at night";
+                    break;
+                case 22:
+                    strHour = "ten";
+                    strMorNoonNight = " at night";
+                    break;
+                case 23:
+                    strHour = "eleven";
+                    strMorNoonNight = " at night";
+                    break;
+                case 24:
+                    strHour = "midnight";
+                    strMorNoonNight = "";
+                    break;
+                default:
+                    strHour = nowish.Hour.ToString();
+                    break;
+            }
+
+            lblTime.Text = strMinute + strHour + strMorNoonNight;
+            label1.Text = strMinute + strHour;
+            notifyIcon1.Text = strMinute + strHour;
+           // output = strMinute + strHour;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
